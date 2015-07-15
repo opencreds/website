c631cc473bba085653516e193d5cadc02bb983da
Gregg Kellogg <gregg@greggkellogg.net>
2015-07-14 17:03:22 -0700

    Update respec in roadmap.

--- specs/opencreds-common.js
+++ specs/opencreds-common.js
@@ -78,6 +78,42 @@ var opencreds = {
     },
     "JSON-PATCH": {
       aliasOf: "json-patch"
+    },
+    'OPEN-BADGES': {
+      title: 'Open Badges',
+      href: 'https://github.com/openbadges/openbadges-specification',
+      authors: ['Brian Brennan', 'Mike Larsson', 'Chris McAvoy', 
+        'Nate Otto', 'Kerri Lemoie'],
+      status:   'BA-DRAFT',
+      publisher:  'Badge Alliance Standard Working Group'
+    },
+    'IDENTITY-CREDENTIALS': {
+      title: 'Identity Credentials',
+      href: 'http://opencreds.org/specs/source/identity-credentials/',
+      authors: ['Manu Sporny', 'Dave Longley'],
+      status:   'CG-DRAFT',
+      publisher:  'W3C Credentials Community Group'
+    },
+    'RDF-NORMALIZATION': {
+      title: 'RDF Dataset Normalization',
+      href: 'http://json-ld.github.io/normalization/spec/',
+      authors: ['Dave Longley', 'Manu Sporny'],
+      status:   'CG-DRAFT',
+      publisher:  'Credentials W3C Community Group'
+    },
+    'WEB-COMMERCE': {
+      title: 'Web Commerce',
+      href: 'https://web-payments.org/specs/source/web-commerce/',
+      authors: ['Dave Longley', 'Manu Sporny', 'David I. Lehn'],
+      status:   'CG-DRAFT',
+      publisher:  'W3C Credentials Community Group'
+    },
+    'WEB-COMMERCE-API': {
+      title: 'Web Commerce API',
+      href: 'https://web-payments.org/specs/source/web-commerce-api/',
+      authors: ['Manu Sporny', 'Dave Longley', 'David I. Lehn'],
+      status:   'CG-DRAFT',
+      publisher:  'W3C Credentials Community Group'
     }
   }
 };
