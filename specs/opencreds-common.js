896a94beb6be8b39b0ae0e7dc4a6c78cf80d234b
Gregg Kellogg <gregg@greggkellogg.net>
2015-07-14 17:08:24 -0700

    Update respec in use-cases. Remove tdef/tref and other old-style respec hacks.

--- specs/opencreds-common.js
+++ specs/opencreds-common.js
@@ -1,54 +1,5 @@
 /* Web Payments Community Group common spec JavaScript */
 var opencreds = {
-  // Add to the respecConfig preProcess config array
-  preProcess: {
-    apply:  function(c) {
-      // process the document before anything else is done
-      var refs = document.querySelectorAll('tdef') ;
-      for (var i = 0; i < refs.length; i++) {
-        var item = refs[i];
-        if (!item) continue ;
-        var p = item.parentNode ;
-        var con = item.innerHTML ;
-        var ref = item.getAttribute('title') ;
-        if (!ref) {
-          ref = item.textContent ;
-        }
-        if (ref) {
-          ref = ref.replace(/\n/g, '_') ;
-          ref = ref.replace(/\s+/g, '_') ;
-        }
-        var sp = document.createElement( 'dfn' ) ;
-        sp.title = ref ;
-        sp.innerHTML = con ;
-        p.replaceChild(sp, item) ;
-      }
-      // now term references
-      refs = document.querySelectorAll('tref') ;
-      for (var i = 0; i < refs.length; i++) {
-        var item = refs[i];
-        if (!item) continue ;
-        var p = item.parentNode ;
-        var con = item.innerHTML ;
-        var ref = item.getAttribute('title') ;
-        if (!ref) {
-          ref = item.textContent ;
-        }
-        if (ref) {
-          ref = ref.replace(/\n/g, '_') ;
-          ref = ref.replace(/\s+/g, '_') ;
-        }
-
-        var sp = document.createElement( 'a' ) ;
-        var id = item.textContent ;
-        sp.className = 'tref' ;
-        sp.title = ref ;
-        sp.innerHTML = con ;
-        p.replaceChild(sp, item) ;
-      }
-    }
-  },
-
   // Add as the respecConfig localBiblio variable
   // Extend or override global respec references
   localBiblio: {
